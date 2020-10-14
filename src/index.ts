727367a2cc4cfe1ef559f0610baa550d00b5e7fe
SamRoehrich <sam_roehrich@icloud.com>
2020-10-14 14:08:59 -0700

    ez clap, timer works

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,69 @@
 import express from "express";
 import http from "http";
 import { Server } from "ws";
-import { createClient, RedisClient } from 'redis'
-
-interface Time {
-  minutes: number
-  seconds: number
-}
+import { createClient } from "async-redis";
 
 (async () => {
-  
   const app = express();
   const server = http.createServer(app);
   const wss = new Server({ server });
   const redisClient = createClient();
-  
+  const publisher = createClient();
+
   // check if there is an error connection to the redis client
   redisClient.on("error", function(error) {
-    console.log(error)
-  })
+    console.log(error);
+  });
 
-  const tick = (time: Time, publisher: RedisClient) => {
-    let newTime: Time = { minutes: 0, seconds: 0}
-    if(time.seconds === 0) {
-      if(time.minutes === 0) {
-        newTime = {minutes: 4, seconds: 0}
-      } else {
-        newTime = {minutes: time.minutes - 1, seconds: 59}
+  async function startTimer() {
+    publisher.publish("scoring:events", "START");
+    await publisher.set("timer:minutes", "4");
+    await publisher.set("timer:seconds", "00");
+    tick();
+  }
+
+  async function tick() {
+    let minutes: any = await redisClient.get("timer:minutes");
+    let seconds: any = await redisClient.get("timer:seconds");
+    publisher.publish("scoring:timer", `${minutes}: ${seconds}`);
+    if (seconds === "00") {
+      if (minutes === "0") {
+        await timerReset();
+      }
+      if (minutes > 0) {
+        await redisClient.set("timer:minutes", String(parseInt(minutes) - 1));
+        await redisClient.set("timer:seconds", "59");
       }
     } else {
-      newTime = {...time, seconds: time.seconds - 1}
+      parseInt(seconds) > 9
+        ? await redisClient.set("timer:seconds", String(seconds - 1))
+        : await redisClient.set("timer:seconds", `0${String(seconds)}`);
     }
-    publisher.publish("scoring:timer", `${newTime.minutes}: ${newTime.seconds}`)
-    // let ticker = setInterval(tick(newTime, publisher), 1000)
-    // publisher.on("message", function(_chanel, message) {
-    //   if(message === "STOP") {
-    //     clearInterval(ticker)
-    //   }
-    // })
-    return ''
-  }
-  
-  function startTimer() {
-    const publisher = createClient()
-    publisher.publish("scoring:events", "START")
-    let time = { 
-      minutes: 4,
-      seconds: 0
-    }
-    tick(time, publisher)
+    setTimeout(tick, 1000);
+    return "";
   }
 
-  
+  async function timerReset() {
+    await redisClient.set("timer:minutes", "4");
+    await redisClient.set("timer:seconds", "00");
+  }
+
   wss.on("connection", function connection(ws) {
-    console.log("Client connected")
+    console.log("Client connected");
     // on websocket connection, sub to scoring chanels
-    redisClient.subscribe("scoring:timer")
-    redisClient.subscribe("scoring:events")
-
+    const sub = createClient();
+    sub.subscribe("scoring:timer");
     // on chanel message, send message over websocket
-    redisClient.on("message", function(_chanel, message) {
-      ws.send(message)
-    })
+    sub.on("message", function(_chanel, message) {
+      ws.send(message);
+    });
 
     // on message sent over websockets
     ws.on("message", function incoming(message) {
       // if message is the START command start the timer
-      if(message === "START") {
-        startTimer()
+      if (message === "START") {
+        startTimer();
       }
-      wss.clients.forEach(function each(client) {
-        if(client.readyState === ws.OPEN) {
-          client.send(message)
-        }
-      })
     });
   });
   server.listen(8080);
